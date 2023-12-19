5d6eb3d68e0b31928961bb563432a87db32279cf
Taofiq-Bakare <taofiq01@gmail.com>
2023-12-19 10:10:01 +0800

    my first commit

---
+++ src/backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"data": "hello world"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
