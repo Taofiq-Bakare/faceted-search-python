10f0fff64bf2da7719099f515a272b4d9fad2797
Taofiq-Bakare <taofiq01@gmail.com>
2023-12-19 23:31:07 +0800

    deleted setup file

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="faceted-search-python",
-    version="0.1.0",
-    packages=find_packages(),  # automatically discover and include all packages
-)
