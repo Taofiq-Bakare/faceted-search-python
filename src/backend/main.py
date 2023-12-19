3d08ab3079ce5db9bed2fbbfe2dc65e4e2e08901
Taofiq-Bakare <taofiq01@gmail.com>
2023-12-19 11:28:22 +0800

    created an entry point for the start command

--- src/backend/main.py
+++ src/backend/main.py
@@ -10,5 +10,5 @@ def home():
     return {"data": "hello world"}
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8081)
